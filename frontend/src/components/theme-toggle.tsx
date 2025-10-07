d6f42126e9ec9ca603f74c72039f9e556dd28e6b
Nguyễn Việt Quốc An <40015342+cloodei@users.noreply.github.com>
2025-10-07 22:15:02 +0700

    reinit

---
+++ frontend/src/components/theme-toggle.tsx
@@ -0,0 +1,37 @@
+import { MoonIcon, SunIcon } from "lucide-react";
+import { Button, type ButtonProps } from "./ui/button";
+import { useTheme } from "./theme-provider";
+import { cn } from "@/lib/utils";
+
+export function ThemeToggler({ className }: ButtonProps) {
+  const { theme, setTheme } = useTheme();
+
+  const switchTheme = () => {
+    if (theme === "dark") {
+      setTheme("light");
+      return;
+    }
+
+    setTheme("dark");
+  };
+
+  const toggleTheme = () => {
+    if (!document.startViewTransition)
+      switchTheme();
+
+    document.startViewTransition(switchTheme);
+  };
+
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      variant="ghost"
+      className={cn("hover:bg-transparent", className)}
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 duration-300 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 duration-300 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
